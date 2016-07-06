3b03bc052b0462248c647fe358978defd1c1cdcd
Nemes Dávid <dokijo@sch.bme.hu>
2016-07-06 19:43:06 +0200

    Karakterkódolási hiba javítása.

--- huto_termosztat/huto_termosztat.ino
+++ huto_termosztat/huto_termosztat.ino
@@ -3,18 +3,18 @@
 #define LM35pin 0
 #define LM35ref 1
 
-unsigned long elozo_millis = 0;					/**< A legutÛbbi iter·l·sn·l rˆgzÌtett idıÈrtÈk milliszekundumban. */
-unsigned long kompresszor_esemeny_millis = 0;	/**< A legutÛbbi kompresszoresemÈny (indÌt·s vagy le·ll·s) ideje milliszekundumban.  */
+unsigned long elozo_millis = 0;					/**< A legut√≥bbi iter√°ls√°n√°l r√∂gz√≠tett id≈ë√©rt√©k milliszekundumban. */
+unsigned long kompresszor_esemeny_millis = 0;	/**< A legut√≥bbi kompresszoresem√©ny (ind√≠t√°s vagy le√°ll√°s) ideje milliszekundumban.  */
 const unsigned long egyperc = 60000.0;			/**< 1 perc (60000 milliszekundum). */
-const unsigned long idokoz = 1000.0; 			/**< 1 m·sodperc idıkˆz, a fıciklusban idızÌtÈsre haszn·ljuk. */
+const unsigned long idokoz = 1000.0; 			/**< 1 m√°sodperc id≈ëk√∂z, a f≈ëciklusban id≈ëz√≠t√©sre haszn√°ljuk. */
 
-const int _RELE_PIN = 13;						/**< A kompresszort kapcsolÛ relÈt meghajtÛ pin a boardon. */
+const int _RELE_PIN = 13;						/**< A kompresszort kapcsol√≥ rel√©t meghajt√≥ pin a boardon. */
 
-const float celhomerseklet = 4.0;				/**< Ezt a hımÈrsÈkletet akarjuk tartani a h˚tıben. */
-const float homerseklet_epszilon = 1.5; 		/**< A hiszterÈzis delt·ja --> Ìgy 2,5-5,5∞C a tartom·ny. */
-float temperature = 0.0;						/**< Aktu·lis hımÈrsÈklet. */
+const float celhomerseklet = 4.0;				/**< Ezt a h≈ëm√©rs√©kletet akarjuk tartani a h≈±t≈ëben. */
+const float homerseklet_epszilon = 1.5; 		/**< A hiszter√©zis delt√°ja --> √≠gy 2,5-5,5¬∞C a tartom√°ny. */
+float temperature = 0.0;						/**< Aktu√°lis h≈ëm√©rs√©klet. */
 
-bool kompresszor = false;						/**< A kompresszor ·llapota. */
+bool kompresszor = false;						/**< A kompresszor √°llapota. */
 
 
 void setup(){
@@ -32,19 +32,19 @@ void loop()
 		elozo_millis = mostani_millis;
 	    temperature = readTemp();
 	    Serial.print(temperature);
-	    Serial.println(" ∞C");
+	    Serial.println(" ÔøΩC");
 	    Serial.print("Futasido: ");
 	    Serial.println(millis() / 1000.0);
 	    Serial.println("-------------");
 
-	    if (mostani_millis - kompresszor_esemeny_millis >= egyperc) {		// Ha a legutÛbbi kompresszoresemÈny Ûta eltelt legal·bb 1 perc
-			if (kompresszor == true) {										// Ha Èppen megy a kompresszor
-				if (temperature <= celhomerseklet - homerseklet_epszilon) { // Ès m·r elÈg hideg van
-					kompresszor_ki();										// akkor kikapcsoljuk a h˚tÈst
+	    if (mostani_millis - kompresszor_esemeny_millis >= egyperc) {		// Ha a legut√≥bbi kompresszoresem√©ny √≥ta eltelt legal√°bb 1 perc
+			if (kompresszor == true) {										// Ha √©ppen megy a kompresszor
+				if (temperature <= celhomerseklet - homerseklet_epszilon) { // √©s m√°r el√©g hideg van
+					kompresszor_ki();										// akkor kikapcsoljuk a h≈±t√©st
 				}
 			} else {														// Ha nem megy a kompresszor
-				if (temperature >= celhomerseklet + homerseklet_epszilon) { // Ès t˙l meleg van
-					kompresszor_be();										// akkor elindÌtjuk a h˚tÈst
+				if (temperature >= celhomerseklet + homerseklet_epszilon) { // √©s t√∫l meleg van
+					kompresszor_be();										// akkor elind√≠tjuk a h≈±t√©st
 				}
 			}
 	    }
